c3f5b188cadd6dee3802a6c634bc2f395df8f1a2
Thomas Shephard <thomas@thomas-shephard.com>
2026-03-28 08:31:23 +0000

    Improve testing efficiency

--- tests/run_privileged_tests.sh
+++ tests/run_privileged_tests.sh
@@ -16,6 +16,6 @@ cargo build --workspace --all-features --bins
 echo "Running Bats full user journey tests..."
 bats -t /workspace/tests/e2e_full_user_path.bats
 
-cargo llvm-cov --workspace --all-features --no-report -- --test-threads=1
+CARGO_INCREMENTAL=0 cargo llvm-cov --workspace --all-features --no-report -- --test-threads=1
 cargo llvm-cov report --cobertura --output-path cobertura.xml
 cargo llvm-cov report --summary-only
